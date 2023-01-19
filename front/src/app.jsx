e5d3d28e263195d68894444dc711f000465bf737
karengomezs <karengomezsoto1@gmail.com>
2023-01-19 10:56:18 -0500

    add file routes

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -1,6 +1,7 @@
 import Carousel from "./components/carousel";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+//ACÁ IMPORTAS ROUTES
 
 function App() {
 	return (
---
+++ front/src/routes/routes.jsx
@@ -0,0 +1,3 @@
+//acá importas loguin, registro y el resto
+//APP SOLO IMPORTA ESTE ARCHIVO ROUTES
+//AQUÍ TODA LA CONFIG DE RECT ROUTER
