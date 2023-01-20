0077675d6bba053226f91437419457d766ac98fd
osnaider.ossoluciones@gmail.com <root@localhost>
2023-01-20 03:04:06 -0500

    Creacion de template bloque buscador. Se deja por finalizar el componente, solo estableciendo lo básico

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,12 @@
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import Searcher from "./components/Searcher";
 import { Route, Routes } from "react-router-dom";
 import Detalle from "./routes/detalle";
 import Main from "./routes/main"
+
+
+
 
 //ACÁ IMPORTAS ROUTES
 
@@ -10,10 +14,13 @@ function App() {
 	return (
 		<>
 			<Navbar />
+			<Searcher/>
 			<Routes>
 				<Route path="/" element={<Main/>}></Route>
 				<Route path="detalle" element={<Detalle />}></Route>
+				
 			</Routes>
+			
 			<Footer />
 		</>
 	);
--- front/src/routes/main.jsx
+++ front/src/routes/main.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Carousel from "../components/carousel";
 
+
+
 export default function Main() {
     return <Carousel />;
 
