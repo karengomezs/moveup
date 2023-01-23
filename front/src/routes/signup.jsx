5873e45de18d24052c7e73d40559b3e53085590c
karengomezs <karengomezsoto1@gmail.com>
2023-01-23 15:50:45 -0500

    fix login layout

--- front/src/routes/signup.jsx
+++ front/src/routes/signup.jsx
@@ -2,7 +2,7 @@ import FormSignup from "../components/form-signup";
 
 export default function Signup() {
   return (
-    <div className="container min-vh-100 d-flex flex-column justify-content-center align-items-center">
+    <div className="container flex-grow-1 d-flex flex-column justify-content-center align-items-center">
       <FormSignup />
     </div>
   );
