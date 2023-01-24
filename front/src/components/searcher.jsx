259c5dc2df79cf6829a2b603b68d7ec0dc8b6f6c
OSNAIDER2022 <osnaider.ossoluciones@gmail.com>
2023-01-24 13:21:29 -0500

    establecimiento de iconos gps_icon y calendar_icon y otros estilos

--- front/src/components/searcher.jsx
+++ front/src/components/searcher.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 /*importe de iconos Font Awesome */
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { faCalendar } from '@fortawesome/free-solid-svg-icons'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+//import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Searcher(){
@@ -11,28 +11,32 @@ export default function Searcher(){
         <>
         <div class="searcher">
             <div class="container text-center">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h1>LOREM IPSUM</h1>  
+                <div class="row">    
+                <div class="col-sm-12">
+
+                        <h1>Lorem Ipsum</h1>  
+        
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-5">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-4">
 
-                        {/*<FontAwesomeIcon icon={faLocationDot}/>*/}
+                        {/*<FontAwesomeIcon icon={faLocationDot} class="calendar_icon"/>*/}
 
                         <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="¿A dónde vamos?"/>
+
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-sm-4">
                         
-                        {/*<FontAwesomeIcon icon={faCalendar}/>*/}
+                        {/*<FontAwesomeIcon icon={faCalendar} class="calendar_icon"/>*/}
                         
                         <input type="text" class="form-control" id="exampleFormControlInput1" placeholder= "Check in - Check out" />
+
                     </div>
                     <div class="col-sm-2">
+
                         <button type="button" class="btn btn-primary">Buscar</button>
         
-                        
                     </div>
                 </div>
             </div>
@@ -40,4 +44,5 @@ export default function Searcher(){
         </>
     );
 }
+
 
