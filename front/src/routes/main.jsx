77026bc54555cd50c63fda026a25b161a7c18e6b
karengomezs <karengomezsoto1@gmail.com>
2023-01-26 13:47:11 -0500

    fix button color

--- front/src/routes/main.jsx
+++ front/src/routes/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Categorias from "../components/categorias";
 import Listado from "../components/listado";
 import Searcher from "../components/searcher";
@@ -7,6 +9,7 @@ export default function Main() {
   return (
     <div className="min-vh-100">
       <Searcher />
+      <Link to="/details">Detalles</Link>
       <Categorias />
       <Listado />
     </div>
