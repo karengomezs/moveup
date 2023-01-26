bbeb6b9f0ea40cdc5fce4f204d1633e77d36f244
OSNAIDER2022 <osnaider.ossoluciones@gmail.com>
2023-01-25 20:23:54 -0500

    correccion de resposive del searcher

--- front/src/components/searcher.jsx
+++ front/src/components/searcher.jsx
@@ -1,10 +1,14 @@
+//import React {useState, useEffect} from "react";
 import React from "react";
 /*importe de iconos Font Awesome */
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-//import { faCalendar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+// import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 
 export default function Searcher() {
+  //declaracion de la API
+
+
   return (
     <>
       <div className="searcher">
@@ -14,26 +18,19 @@ export default function Searcher() {
               <h1>Lorem Ipsum</h1>
             </div>
           </div>
-          <div className="row justify-content-md-center">
+          <div className="row justify-content-md-center input-group">
             <div className="col-sm-4">
-              {/*<FontAwesomeIcon icon={faLocationDot} className="calendar_icon"/>*/}
-
-              <input
-                type="text"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="¿A dónde vamos?"
-              />
+            <div class="input-group">
+                <span class="input-group-text">{<FontAwesomeIcon icon={faLocationDot} className="calendar_icon"/>}</span>
+                <input type="text" aria-label="Last name" class="form-control" placeholder="¿A donde vamos ir?"/>
+              </div>
             </div>
             <div className="col-sm-4">
-              {/*<FontAwesomeIcon icon={faCalendar} className="calendar_icon"/>*/}
-
-              <input
-                type="text"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Check in - Check out"
-              />
+              <div class="input-group">
+                {/* {<span class="input-group-text">{<FontAwesomeIcon icon={faCalendar} className="calendar_icon"/>}</span>} */}
+                <input type="date" aria-label="First name" class="form-control" placeholder="Check in"/>
+                <input type="date" aria-label="Last name" class="form-control" placeholder="Check out"/>
+              </div>
             </div>
             <div className="col-sm-2">
               <button type="button" className="btn btn-primary">
