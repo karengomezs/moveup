83be8e172af395cf01e7ba1ac3a0f097f8278471
OSNAIDER2022 <osnaider.ossoluciones@gmail.com>
2023-01-25 23:03:26 -0500

    creacion de archivo cities.json y preparacion de entorno para API-FAKE para simular peticiones

--- front/src/components/searcher.jsx
+++ front/src/components/searcher.jsx
@@ -1,12 +1,24 @@
-//import React {useState, useEffect} from "react";
-import React from "react";
+
+import React, { useState } from "react";
 /*importe de iconos Font Awesome */
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 // import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 
+
+
 export default function Searcher() {
-  //declaracion de la API
+  //declaracion de state
+  const [form, stepForm] = useState(initialForm)
+  //declaacion de funcion Handle
+  const handleChange = (e) =>{}
+  const handleSubmit = (e) =>{}
+  
+  const initialForm = {
+    id: null,
+    name:""
+  };
+
 
 
   return (
@@ -21,8 +33,8 @@ export default function Searcher() {
           <div className="row justify-content-md-center input-group">
             <div className="col-sm-4">
             <div class="input-group">
-                <span class="input-group-text">{<FontAwesomeIcon icon={faLocationDot} className="calendar_icon"/>}</span>
-                <input type="text" aria-label="Last name" class="form-control" placeholder="¿A donde vamos ir?"/>
+                {/* <span class="input-group-text">{<FontAwesomeIcon icon={faLocationDot} className="calendar_icon"/>}</span> */}
+                <input type="text" aria-label="Last name" class="form-control" placeholder="¿A donde vamos ir?" onChange={handleChange} value="form.city"/>
               </div>
             </div>
             <div className="col-sm-4">
@@ -33,7 +45,7 @@ export default function Searcher() {
               </div>
             </div>
             <div className="col-sm-2">
-              <button type="button" className="btn btn-primary">
+              <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
                 Buscar
               </button>
             </div>
