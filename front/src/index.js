02bc8f4ae5f09aabd5fea49eeb7a26aa032d65ba
uancamilo <uancamilo@gmail.com>
2023-01-26 20:07:16 -0500

    boostrap icons agregado y cambiado los svg

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap";
 import "./components/styles/global.scss";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
