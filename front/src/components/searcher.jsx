f5c2a138e2fbda860b8b6184e388978afd7f85fb
uancamilo <uancamilo@gmail.com>
2023-01-27 12:09:08 -0500

    cambios de css a bootstrap

--- front/src/components/searcher.jsx
+++ front/src/components/searcher.jsx
@@ -1,52 +1,55 @@
-
-import React  from "react";
-/*importe de iconos Font Awesome */
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-// import { faCalendar } from '@fortawesome/free-solid-svg-icons'
-
+import React from "react";
 
 export default function Searcher() {
-  
-  
-  return (
-    <>
-      <div className="searcher">
-      <form>
-        <div className="container text-center">
-          <div className="row">
-            <div className="col-sm-12">
-              <h1>Lorem Ipsum</h1>
-            </div>
-          </div>
-          <div className="row justify-content-md-center input-group">
-            <div className="col-sm-4">
-            <div class="input-group">
-                {/* <span class="input-group-text"><i class="bi bi-geo-alt"></i></span> */}
-                <select class="form-select">
-                  <option selected> ¿A donde vamos ir? </option>
-                  <option value="1">Medellin</option>
-                  <option value="2">Barranquilla</option>
-                  <option value="3">Bogota</option>
-                  <option value="4">Cali</option>
-                </select>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div class="input-group">
-                {/* {<span class="input-group-text">{<FontAwesomeIcon icon={faCalendar} className="calendar_icon"/>}</span>} */}
-                <input type="date" aria-label="checkIn" class="form-control" placeholder="Check in"/>
-                <input type="date" aria-label="checkOut" class="form-control" placeholder="Check out"/>
-              </div>
-            </div>
-            <div className="col-sm-2">
-              <button type="submit" className="btn btn-primary">Buscar</button>
-            </div>
-          </div>
-        </div>
-        </form>
-      </div>
-    </>
-  );
-
+	return (
+		<>
+			<div className="bg-buscador py-5">
+				<div className="d-flex justify-content-center">
+					<h1 className="fs-2 fw-bold text-light">Lorem Ipsum</h1>
+				</div>
+				<form>
+					<div className="container text-center">
+						<div className="row justify-content-center input-group">
+							<div className="col col-sm-12 col-lg-5 p-2">
+								<div className="input-group">
+									<select className="form-select border border-0 shadow-none">
+										<option value="" selected>
+											¿A donde vamos ir?
+										</option>
+										<option value="1">Medellin</option>
+										<option value="2">Barranquilla</option>
+										<option value="3">Bogota</option>
+										<option value="4">Cali</option>
+									</select>
+								</div>
+							</div>
+							<div className="col col-sm-12 col-lg-5 p-2">
+								<div className="input-group">
+									<input
+										type="date"
+										aria-label="checkIn"
+										className="form-control border border-0 shadow-none"
+										placeholder="Check in"
+									/>
+									<input
+										type="date"
+										aria-label="checkOut"
+										className="form-control border border-0 shadow-none"
+										placeholder="Check out"
+									/>
+								</div>
+							</div>
+							<div className="col p-2">
+								<div className="d-grid">
+									<button type="submit" className="btn boton-busqueda">
+										Buscar
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
