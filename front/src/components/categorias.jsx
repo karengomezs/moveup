7837a94c24ef00c25cd2818a05c0dbc3da90fbbb
karengomezs <karengomezsoto1@gmail.com>
2023-02-01 19:23:14 -0500

    categories ok with back data

--- front/src/components/categorias.jsx
+++ front/src/components/categorias.jsx
@@ -1,56 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCategories } from "../api/categories";
 
 export default function Categorias() {
-	const categorias = [
-		{
-			id: 1,
-			nombre: "Hoteles",
-			cantidad: 45965,
-			image: "https://picsum.photos/200/198",
-		},
+  const [data, setData] = useState([]);
 
-		{
-			id: 2,
-			nombre: "Hostales",
-			cantidad: 9865,
-			image: "https://picsum.photos/200/200",
-		},
-		{
-			id: 3,
-			nombre: "Apartamentos",
-			cantidad: 18783,
-			image: "https://picsum.photos/200/199",
-		},
-		{
-			id: 4,
-			nombre: "Bed & Breakfast",
-			cantidad: 3992,
-			image: "https://picsum.photos/200/201",
-		},
-	];
+  useEffect(() => {
+    getCategories().then((data) => {
+      setData(data);
+    });
+  }, []);
 
-	return (
-		<div className="container pt-5">
-			<p className="fs-4 fw-bold">Buscar por tipo de alojamiento</p>
-			<div className="cards">
-				{categorias.map((categoria) => (
-					<div key={categoria.id} className="cards-border">
-						<div className="d-flex justify-content-center w-100">
-							<img
-								className="cards-image"
-								src={categoria.image}
-								alt={categoria.nombre}
-							/>
-						</div>
-						<div>
-							<h5 className="fw-bolder pt-2 px-3">{categoria.nombre}</h5>
-							<p className="px-3">
-								{categoria.cantidad} <span>hoteles</span>
-							</p>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  const eachCategory = data.map((category) => {
+    return (
+      <div key={category.id} className="cards-border d-flex flex-column">
+        <div className="d-flex justify-content-center w-100 flex-grow-1">
+          <img
+            className="cards-image object-fit-cover"
+            src={category.url}
+            alt={category.nombre}
+          />
+        </div>
+        <div>
+          <h5 className="fw-bolder pt-2 px-3">{category.nombre}</h5>
+          <p className="px-3">{category.descripcion}</p>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="container pt-5">
+      <p className="fs-4 fw-bold">Buscar por tipo de alojamiento</p>
+      <div className="cards">{eachCategory}</div>
+    </div>
+  );
 }
--- front/src/components/searcher.jsx
+++ front/src/components/searcher.jsx
@@ -1,55 +1,59 @@
 import React from "react";
 
 export default function Searcher() {
-	return (
-		<>
-			<div className="bg-buscador py-5">
-				<div className="d-flex justify-content-center">
-					<h1 className="fs-2 fw-bold text-light">Lorem Ipsum</h1>
-				</div>
-				<form>
-					<div className="container d-flex justify-content-center">
-						<div className="row justify-content-center input-group">
-							<div className="col-sm-12 col-md-5 p-2">
-								<div className="input-group">
-									<select className="form-select border border-0 shadow-none">
-										<option value="" selected>
-											¿A donde vamos ir?
-										</option>
-										<option value="1">Medellin</option>
-										<option value="2">Barranquilla</option>
-										<option value="3">Bogota</option>
-										<option value="4">Cali</option>
-									</select>
-								</div>
-							</div>
-							<div className="col-sm-12 col-md-5 p-2">
-								<div className="input-group">
-									<input
-										type="date"
-										aria-label="checkIn"
-										className="form-control border border-0 shadow-none"
-										placeholder="Check in"
-									/>
-									<input
-										type="date"
-										aria-label="checkOut"
-										className="form-control border border-0 shadow-none"
-										placeholder="Check out"
-									/>
-								</div>
-							</div>
-							<div className="col p-2">
-								<div className="d-grid">
-									<button type="submit" className="btn boton-busqueda">
-										Buscar
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="bg-buscador py-5">
+        <div className="d-flex justify-content-center">
+          <h1 className="fs-2 fw-bold text-light">Lorem Ipsum</h1>
+        </div>
+        <form>
+          <div className="container d-flex justify-content-center">
+            <div className="row justify-content-center input-group">
+              <div className="col-sm-12 col-md-5 p-2">
+                <div className="input-group">
+                  <select
+                    onChange={() => {}}
+                    value={"0"}
+                    className="form-select border border-0 shadow-none"
+                  >
+                    <option disabled value="0">
+                      ¿A donde vamos ir?
+                    </option>
+                    <option value="1">Medellin</option>
+                    <option value="2">Barranquilla</option>
+                    <option value="3">Bogota</option>
+                    <option value="4">Cali</option>
+                  </select>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-5 p-2">
+                <div className="input-group">
+                  <input
+                    type="date"
+                    aria-label="checkIn"
+                    className="form-control border border-0 shadow-none"
+                    placeholder="Check in"
+                  />
+                  <input
+                    type="date"
+                    aria-label="checkOut"
+                    className="form-control border border-0 shadow-none"
+                    placeholder="Check out"
+                  />
+                </div>
+              </div>
+              <div className="col p-2">
+                <div className="d-grid">
+                  <button type="submit" className="btn boton-busqueda">
+                    Buscar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
