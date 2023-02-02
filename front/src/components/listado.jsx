62fd6c2f3fee7eb1d4952dd1e29d8c25f61289a0
karengomezs <karengomezsoto1@gmail.com>
2023-02-02 17:01:11 -0500

    little change

--- front/src/components/listado.jsx
+++ front/src/components/listado.jsx
@@ -1,133 +1,162 @@
 import React from "react";
 
-const listado = [
-	{
-		id: 1,
-		categoria: "Hotel",
-		nombre: "Hotel Realeza",
-		ubicacion: "Laureles, Medellin, CO",
-		descripcion:
-			"Hotel con todos los servicios incluidos, zonas humedas y bar-restaurante con platos a la carta",
-		img: "https://picsum.photos/200/400",
-	},
-	{
-		id: 2,
-		categoria: "Hostal",
-		nombre: "El Paraiso",
-		ubicacion: "Vía San Geronimo - Medellín Km 15",
-		descripcion:
-			"Perfecto para un descanso y dias soleados incluye el desayuno y servicio a la habitación",
-		img: "https://picsum.photos/199/400",
-	},
-	{
-		id: 3,
-		categoria: "Apartamento",
-		nombre: "Apartaestudio en Laureles",
-		ubicacion: "Calle 35 # 76-114 Laureles- Estado",
-		descripcion: "Comodo y central",
-		img: "https://picsum.photos/201/400",
-	},
-	{
-		id: 4,
-		categoria: "Bed & Breakfast",
-		nombre: "Habitacion con wifi y baño privado",
-		ubicacion: "Carrera 39D #47Sur9",
-		descripcion: "AirB&B en Envigado",
-		img: "https://picsum.photos/200/399",
-	},
-	{
-		id: 5,
-		categoria: "Hotel",
-		nombre: "Hotel El Bosque",
-		ubicacion: "Carrera 23 # 36ASur27. Envigado, Antioquia",
-		descripcion:
-			"Dormitorios cálidos, algunos con balcón y vistas rurales, en pintoresco hotel con restaurante y una granja.",
-		img: "https://picsum.photos/200/401",
-	},
-	{
-		id: 6,
-		categoria: "Hostal",
-		nombre: "Manantial",
-		ubicacion: "Carrera 50 #87-29, San Fernando, Itagüi, Antioquia",
-		descripcion: "Excelente precio y servicio",
-		img: "https://picsum.photos/199/399",
-	},
-	{
-		id: 7,
-		categoria: "Apartamento",
-		nombre: "Apartamento Amoblado",
-		ubicacion: "Barrio Provenza Calle 8av #39-151",
-		descripcion: "Cerca de la zona rosa",
-		img: "https://picsum.photos/201/401",
-	},
-	{
-		id: 8,
-		categoria: "Bed & Breakfast",
-		nombre: "Habitacion acojedora desayuno incluido",
-		ubicacion: "Calle 10 #41-21, El Poblado, Medellín, Antioquia",
-		descripcion:
-			"Este hotel sencillo se encuentra a 3 minutos a pie del parque Lleras y su vida nocturna.",
-		img: "https://picsum.photos/199/401",
-	},
+// producto={
+// 	calificación: (de 1 a 5) renderizar LP
+// 	categoria: renderizar LP
+// 	nombreClase: renderizar LP
+// 	ubicacionClase: renderizar LP
+// 	descripcionClase:(pequeña) renderizar LP
+// 	profesorDisponible: renderizar LP
+// 	fechasDisponibles: detalles
+// 	horariosDisponibles: detalles
+// 	}
+
+const producto = [
+  {
+    id: 1,
+    calificación: 4,
+    categoria: "Acuáticos",
+    nombreClase: "Surf",
+    ubicacionClase: "Cartagena CO",
+    descripcionClase:
+      "deporte que se realiza en el mar ayuda de una tabla y un profesor",
+    profesorDisponible: "Jairo",
+    // fechasDisponibles: detalles,
+    // horariosDisponibles: detalles,
+    img: "https://picsum.photos/200/400",
+  },
+  {
+    id: 2,
+    calificación: 4,
+    categoria: "Treckking",
+    nombreClase: "Caminata Escalera",
+    ubicacionClase: "Jardin CO",
+    descripcionClase: "camina por las montañas de Jardín con un grupo",
+    profesorDisponible: "Camila",
+    img: "https://picsum.photos/199/400",
+  },
+  {
+    id: 3,
+    calificación: 5,
+    categoria: "Artes Marciales",
+    nombreClase: "Tekondo",
+    ubicacionClase: "Medellín CO",
+    descripcionClase:
+      "arte y deporte de origen oriental en el que se aprende defensa personal",
+    profesorDisponible: "Joshua",
+    img: "https://picsum.photos/201/400",
+  },
+  {
+    id: 4,
+    calificación: 4,
+    categoria: "Aeróbicos",
+    nombreClase: "Rumba",
+    ubicacionClase: "Armenia CO",
+    descripcionClase: "Baila al ritmo de la música mientras te pones saludable",
+    profesorDisponible: "Angélica",
+    img: "https://picsum.photos/200/399",
+  },
+  {
+    id: 5,
+    calificación: 5,
+    categoria: "Acuáticos",
+    nombreClase: "Remo",
+    ubicacionClase: "Amazonas CO",
+    descripcionClase: "Navega en el río Amazonas con tu compañero favorito",
+    profesorDisponible: "Oscar",
+    img: "https://picsum.photos/200/401",
+  },
+  {
+    id: 6,
+    calificación: 4,
+    categoria: "Aeróbicos",
+    nombreClase: "GAP",
+    ubicacionClase: "Cartagena CO",
+    descripcionClase:
+      "Realiza actividades de cardio para estar en forma y alerta",
+    profesorDisponible: "Jairo",
+    img: "https://picsum.photos/199/399",
+  },
+  {
+    id: 7,
+    calificación: 5,
+    categoria: "Trekking",
+    nombreClase: "Rumbo a la sierra nevada",
+    ubicacionClase: "Santa Martha CO",
+    descripcionClase:
+      "Recorrido en montaña y pendientes retadoras, es para ti?",
+    profesorDisponible: "Chimuelo",
+    img: "https://picsum.photos/201/401",
+  },
+  {
+    id: 8,
+    calificación: 5,
+    categoria: "Artes Marciales",
+    nombreClase: "Yuyitsu",
+    ubicacionClase: "Cali CO",
+    descripcionClase: "Deporte de combate y sistema de defensa personal",
+    profesorDisponible: "Hank",
+    img: "https://picsum.photos/199/401",
+  },
 ];
 
 export default function Listado() {
-	return (
-		<div className="container py-5">
-			<p className="fs-4 fw-bold">Recomendaciones</p>
-			<div className="anchoLista">
-				{listado.map((itemListado) => (
-					<div key={itemListado.id} className="card mb-3 anchoLista">
-						<div className="row g-0 shadow">
-							<div className="col-3">
-								<img
-									src={itemListado.img}
-									className="img-fluid rounded-start"
-									alt={itemListado.nombre}
-								/>
-							</div>
-							<div className="col-9">
-								<div className="card-body h-100 d-flex flex-column justify-content-between">
-									<div className="d-flex justify-content-between">
-										<div className="mb-2">
-											<div className="d-flex align-items-end">
-												<h6 className="m-0 pe-1 text-muted">
-													{itemListado.categoria}
-												</h6>
-												<div className="text-primary">
-													<i className="bi bi-star-fill" />
-													<i className="bi bi-star-fill" />
-													<i className="bi bi-star-fill" />
-													<i className="bi bi-star-fill" />
-													<i className="bi bi-star-fill" />
-												</div>
-											</div>
-											<h5 className="card-title">{itemListado.nombre}</h5>
-										</div>
-										<div className="text-end">
-											<i className="bi bi-8-square-fill fs-3" />
-											<p className="text-nowrap">Muy bueno</p>
-										</div>
-									</div>
-									<div className="d-flex justify-content-around">
-										<i className="bi bi-geo-alt-fill fs-3" />
-										<div className="d-flex justify-content-between">
-											<p className="card-text">{itemListado.ubicacion}</p>&nbsp;
-											<a className="text-nowrap" href="/">
-												Mostrar ubicación
-											</a>
-										</div>
-									</div>
-									<p className="card-text">{itemListado.descripcion}</p>
-									<div className="d-grid align-items-end">
-										<button className="btn btn-outline-primary">ver mas</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="container py-5">
+      <p className="fs-4 fw-bold">Recomendaciones</p>
+      <div className="anchoLista">
+        {listado.map((itemListado) => (
+          <div key={itemListado.id} className="card mb-3 anchoLista">
+            <div className="row g-0 shadow">
+              <div className="col-3">
+                <img
+                  src={itemListado.img}
+                  className="img-fluid rounded-start"
+                  alt={itemListado.nombre}
+                />
+              </div>
+              <div className="col-9">
+                <div className="card-body h-100 d-flex flex-column justify-content-between">
+                  <div className="d-flex justify-content-between">
+                    <div className="mb-2">
+                      <div className="d-flex align-items-end">
+                        <h6 className="m-0 pe-1 text-muted">
+                          {itemListado.categoria}
+                        </h6>
+                        <div className="text-primary">
+                          <i className="bi bi-star-fill" />
+                          <i className="bi bi-star-fill" />
+                          <i className="bi bi-star-fill" />
+                          <i className="bi bi-star-fill" />
+                          <i className="bi bi-star-fill" />
+                        </div>
+                      </div>
+                      <h5 className="card-title">{itemListado.nombre}</h5>
+                    </div>
+                    <div className="text-end">
+                      <i className="bi bi-8-square-fill fs-3" />
+                      <p className="text-nowrap">Muy bueno</p>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-around">
+                    <i className="bi bi-geo-alt-fill fs-3" />
+                    <div className="d-flex justify-content-between">
+                      <p className="card-text">{itemListado.ubicacion}</p>&nbsp;
+                      <a className="text-nowrap" href="/">
+                        Mostrar ubicación
+                      </a>
+                    </div>
+                  </div>
+                  <p className="card-text">{itemListado.descripcion}</p>
+                  <div className="d-grid align-items-end">
+                    <button className="btn btn-outline-primary">ver mas</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
