7c38d421cf9d7b9d8e0bb9cfe208246ba92f7a8a
karengomezs <karengomezsoto1@gmail.com>
2023-02-02 19:13:42 -0500

    listado object, classes ok render

--- front/src/components/listado.jsx
+++ front/src/components/listado.jsx
@@ -20,7 +20,7 @@ const producto = [
     nombreClase: "Surf",
     ubicacionClase: "Cartagena CO",
     descripcionClase:
-      "deporte que se realiza en el mar ayuda de una tabla y un profesor",
+      "Deporte que se realiza en el mar con ayuda de una tabla y un profesor",
     profesorDisponible: "Jairo",
     // fechasDisponibles: detalles,
     // horariosDisponibles: detalles,
@@ -32,7 +32,7 @@ const producto = [
     categoria: "Treckking",
     nombreClase: "Caminata Escalera",
     ubicacionClase: "Jardin CO",
-    descripcionClase: "camina por las montañas de Jardín con un grupo",
+    descripcionClase: "Camina por las montañas de Jardín con un grupo",
     profesorDisponible: "Camila",
     // fechasDisponibles: detalles,
     // horariosDisponibles: detalles,
@@ -45,7 +45,7 @@ const producto = [
     nombreClase: "Tekondo",
     ubicacionClase: "Medellín CO",
     descripcionClase:
-      "arte y deporte de origen oriental en el que se aprende defensa personal",
+      "Arte y deporte de origen oriental en el que se aprende defensa personal",
     profesorDisponible: "Joshua",
     // fechasDisponibles: detalles,
     // horariosDisponibles: detalles,
@@ -116,70 +116,71 @@ const producto = [
 ];
 
 export default function Listado() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    getCategories().then((data) => {
-      setData(data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   getCategories().then((data) => {
+  //     setData(data);
+  //   });
+  // }, []);
+
+  const eachClass = producto.map((eachClass) => {
+    return (
+      <div key={eachClass.id} className="card mb-3 anchoLista">
+        <div className="row g-0 shadow">
+          <div className="col-3">
+            <img src={eachClass.img} className="img-fluid rounded-start" />
+          </div>
+          <div className="col-9">
+            <div className="card-body h-100 d-flex flex-column justify-content-between">
+              <div className="d-flex justify-content-between">
+                <div className="mb-2">
+                  <div className="d-flex align-items-end">
+                    <h6 className="m-0 pe-1 text-muted">
+                      {"Categoría: "}
+                      {eachClass.categoria}
+                    </h6>
+                    {/* <div className="text-primary">
+                      <i className="bi bi-star-fill" />
+                      <i className="bi bi-star-fill" />
+                      <i className="bi bi-star-fill" />
+                      <i className="bi bi-star-fill" />
+                      <i className="bi bi-star-fill" />
+                    </div> */}
+                  </div>
+                  <h5 className="card-title">{eachClass.nombreClase}</h5>
+                </div>
+                <div className="text-end">
+                  {/* <i className="bi bi-8-square-fill fs-3" /> */}
+                  <h6 className="text-nowrap">
+                    Calificación: {eachClass.calificación}
+                  </h6>
+                </div>
+              </div>
+              <div className="d-flex justify-content-around">
+                <i className="bi bi-geo-alt-fill fs-3" />
+                <div className="d-flex justify-content-between">
+                  <p className="card-text">{eachClass.ubicacionClase}</p>&nbsp;
+                  <a className="text-nowrap" href="/">
+                    Mostrar ubicación
+                  </a>
+                </div>
+              </div>
+              <p className="card-text">{eachClass.descripcionClase}</p>
+              <div className="d-grid align-items-end">
+                <button className="btn btn-outline-primary">ver mas</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="container py-5">
       <p className="fs-4 fw-bold">Recomendaciones</p>
-      <div className="anchoLista">
-        {listado.map((itemListado) => (
-          <div key={itemListado.id} className="card mb-3 anchoLista">
-            <div className="row g-0 shadow">
-              <div className="col-3">
-                <img
-                  src={itemListado.img}
-                  className="img-fluid rounded-start"
-                  alt={itemListado.nombre}
-                />
-              </div>
-              <div className="col-9">
-                <div className="card-body h-100 d-flex flex-column justify-content-between">
-                  <div className="d-flex justify-content-between">
-                    <div className="mb-2">
-                      <div className="d-flex align-items-end">
-                        <h6 className="m-0 pe-1 text-muted">
-                          {itemListado.categoria}
-                        </h6>
-                        <div className="text-primary">
-                          <i className="bi bi-star-fill" />
-                          <i className="bi bi-star-fill" />
-                          <i className="bi bi-star-fill" />
-                          <i className="bi bi-star-fill" />
-                          <i className="bi bi-star-fill" />
-                        </div>
-                      </div>
-                      <h5 className="card-title">{itemListado.nombre}</h5>
-                    </div>
-                    <div className="text-end">
-                      <i className="bi bi-8-square-fill fs-3" />
-                      <p className="text-nowrap">Muy bueno</p>
-                    </div>
-                  </div>
-                  <div className="d-flex justify-content-around">
-                    <i className="bi bi-geo-alt-fill fs-3" />
-                    <div className="d-flex justify-content-between">
-                      <p className="card-text">{itemListado.ubicacion}</p>&nbsp;
-                      <a className="text-nowrap" href="/">
-                        Mostrar ubicación
-                      </a>
-                    </div>
-                  </div>
-                  <p className="card-text">{itemListado.descripcion}</p>
-                  <div className="d-grid align-items-end">
-                    <button className="btn btn-outline-primary">ver mas</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className="anchoLista">{eachClass}</div>
     </div>
   );
 }
