568073aa5e03ae5ce30a995f9e8114373d4f9384
karengomezs <karengomezsoto1@gmail.com>
2023-02-03 19:43:27 -0500

    filter ticket 34

--- front/src/routes/main.jsx
+++ front/src/routes/main.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "../components/categories";
 import List from "../components/list";
 import Searcher from "../components/searcher";
+import { getClasses } from "../api/products";
 
 export default function Main() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getClasses().then((data) => {
+      setData(data);
+    });
+  }, []);
+
   return (
     <div className="min-vh-100">
-      <Searcher />
+      <Searcher
+        onSearch={(city, date) => {
+          getClasses(city, date).then((data) => {
+            setData(data);
+          });
+        }}
+      />
       <Categories />
-      <List />
+      <List data={data} />
     </div>
   );
 }
