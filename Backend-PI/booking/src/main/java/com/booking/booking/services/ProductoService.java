be7753d13144e1b7eeb116a7a00f63df7e12de81
karen gómez soto <karengomezsoto1@gmail.com>
2023-02-10 19:57:04 +0000

    Random service

--- Backend-PI/booking/src/main/java/com/booking/booking/services/ProductoService.java
+++ Backend-PI/booking/src/main/java/com/booking/booking/services/ProductoService.java
@@ -1,12 +1,11 @@
 package com.booking.booking.services;
 
-import com.booking.booking.entities.Categoria;
 import com.booking.booking.entities.Producto;
 import com.booking.booking.repositories.ProductosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,7 +30,19 @@ public class ProductoService {
     public void delete (Long id){ productosRepository.deleteById(id);}
 
     public List<Producto> getByCiudad(String ciudad){
-        List<Producto> productoList=productosRepository.findByUbicaciónClase(ciudad);
+        List<Producto> productoList = productosRepository.findByUbicaciónClase(ciudad);
         return productoList;
+    }
+
+    public List<Producto> getRecomendados() {
+        String[] propiedades = {"descripcionClase", "ciudadId", "entrenadorId", "nombreClase", "fechaDisponible"};
+        int rand = getRandomNumber(0, propiedades.length - 1);
+        String propiedad = propiedades[rand];
+
+        return productosRepository.findAll(Sort.by(Sort.Direction.DESC, propiedad));
+    }
+
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
--- front/src/routes/main.jsx
+++ front/src/routes/main.jsx
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Categories from "../components/categories";
 import List from "../components/list";
 import Searcher from "../components/searcher";
-import { getClasses } from "../api/products";
+import { getClasses, getRecomendados } from "../api/products";
+import UserContext from "../context/user-context";
 
 export default function Main() {
+  const userState = useContext(UserContext);
   const [data, setData] = useState([]);
 
+  const userIsLog = Boolean(userState.user);
+
   useEffect(() => {
-    getClasses().then((data) => {
-      setData(data);
-    });
-  }, []);
+    if (userIsLog) {
+      getClasses().then((data) => {
+        setData(data);
+      });
+    } else {
+      getRecomendados().then((data) => {
+        setData(data);
+      });
+    }
+  }, [userIsLog]);
 
   return (
     <div className="min-vh-100">
