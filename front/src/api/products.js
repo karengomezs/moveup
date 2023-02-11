379f0f2ed6c9f69031c5fdaed0869b63624fc222
karengomezs <karengomezsoto1@gmail.com>
2023-02-11 18:06:28 -0500

    filter by category

--- front/src/api/products.js
+++ front/src/api/products.js
@@ -1,4 +1,4 @@
-import { host } from '../constants';
+import { host } from "../constants";
 export const apiUrl = `${host}/producto`;
 
 export async function getClass(id) {
@@ -24,6 +24,18 @@ export async function getRecommended() {
   }
 }
 
+export async function getClassByCategory(id) {
+  const url = `${apiUrl}/buscar/categoria/${id}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export async function getClasses() {
   try {
     const response = await fetch(apiUrl);
@@ -35,8 +47,8 @@ export async function getClasses() {
 }
 
 export async function getFilteredClasses(
-  ciudad = '',
-  date = { start: '', end: '' }
+  ciudad = "",
+  date = { start: "", end: "" }
 ) {
   const params = new URLSearchParams({
     ciudad,
--- front/src/routes/main.jsx
+++ front/src/routes/main.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { format, isValid } from 'date-fns';
-import Categories from '../components/categories';
-import List from '../components/list';
-import Searcher from '../components/searcher';
+import React, { useEffect, useState, useContext } from "react";
+import { format, isValid } from "date-fns";
+import Categories from "../components/categories";
+import List from "../components/list";
+import Searcher from "../components/searcher";
 import {
   getClasses,
   getRecommended,
   getFilteredClasses,
-} from '../api/products';
-import UserContext from '../context/user-context';
+  getClassByCategory,
+} from "../api/products";
+import UserContext from "../context/user-context";
 
 export default function Main() {
   const userState = useContext(UserContext);
@@ -32,20 +33,20 @@ export default function Main() {
     <div className="min-vh-100">
       <Searcher
         onSearch={(city, date) => {
-          const formatDate = 'yyyy-MM-dd';
+          const formatDate = "yyyy-MM-dd";
 
           const start = isValid(date.start)
             ? format(date.start, formatDate)
-            : '';
+            : "";
 
-          const end = isValid(date.end) ? format(date.end, formatDate) : '';
+          const end = isValid(date.end) ? format(date.end, formatDate) : "";
 
           getFilteredClasses(city, { start, end }).then((data) => {
             setData(data);
           });
         }}
       />
-      <Categories />
+      <Categories onClickCategory={setData} />
       <List data={data} />
     </div>
   );
