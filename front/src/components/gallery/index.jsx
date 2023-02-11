5501f20c92b922998e84283d9d9ce4ffd3596214
karengomezs <karengomezs0601@gmail.com>
2023-02-11 14:22:18 -0500

    Fix gallery style

--- front/src/components/gallery/index.jsx
+++ front/src/components/gallery/index.jsx
@@ -3,13 +3,6 @@ import styles from './styles.module.scss';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 
 const Grid = () => {
-  const smallItemStyles = {
-    cursor: 'pointer',
-    objectFit: 'cover',
-    width: '100%',
-    maxHeight: '100%',
-  };
-
   return (
     <Gallery>
       <div className={styles.container}>
@@ -39,7 +32,7 @@ const Grid = () => {
         >
           {({ ref, open }) => (
             <img
-              style={smallItemStyles}
+              className={styles.item}
               src="https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_m.jpg"
               ref={ref}
               onClick={open}
@@ -56,7 +49,7 @@ const Grid = () => {
         >
           {({ ref, open }) => (
             <img
-              style={smallItemStyles}
+              className={styles.item}
               src="https://farm4.staticflickr.com/3902/14985871946_86abb8c56f_m.jpg"
               ref={ref}
               onClick={open}
@@ -73,7 +66,7 @@ const Grid = () => {
         >
           {({ ref, open }) => (
             <img
-              style={{ ...smallItemStyles, gridColumnStart: 2 }}
+              className={styles.third}
               src="https://farm6.staticflickr.com/5584/14985868676_4b802b932a_m.jpg"
               ref={ref}
               onClick={open}
@@ -90,7 +83,7 @@ const Grid = () => {
         >
           {({ ref, open }) => (
             <img
-              style={smallItemStyles}
+              className={styles.item}
               src="https://farm4.staticflickr.com/3920/15008465772_383e697089_m.jpg"
               ref={ref}
               onClick={open}
