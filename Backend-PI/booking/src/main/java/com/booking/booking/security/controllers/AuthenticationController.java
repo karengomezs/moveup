92bc22378a215f60157d42e5a4402e4d9ddf9a85
karen gómez soto <karengomezsoto1@gmail.com>
2023-02-20 15:14:02 +0000

    validacion de registro de email de usuarios, y retorno de Id de los usuarios...

--- Backend-PI/booking/src/main/java/com/booking/booking/security/controllers/AuthenticationController.java
+++ Backend-PI/booking/src/main/java/com/booking/booking/security/controllers/AuthenticationController.java
@@ -3,9 +3,17 @@ package com.booking.booking.security.controllers;
 import com.booking.booking.security.entities.AuthenticationRequest;
 import com.booking.booking.security.entities.AuthenticationResponse;
 import com.booking.booking.security.entities.RegisterRequest;
+import com.booking.booking.security.entities.Usuario;
+import com.booking.booking.security.repositories.UsuarioRepository;
 import com.booking.booking.security.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +23,20 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    private final UsuarioRepository repository;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ){
-        return ResponseEntity.ok(service.register(request));
+        Optional<Usuario> user=repository.findByEmail(request.getEmail());
+
+        if (user.isPresent()){
+            return ResponseEntity.notFound().build();
+        }else {
+            return ResponseEntity.ok(service.register(request));
+        }
+
     }
 
     @PostMapping("/authenticate")
