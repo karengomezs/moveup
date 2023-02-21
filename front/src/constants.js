67309a70940a16ecc53eb2cd57da5337ee75539c
karen gómez soto <karengomezsoto1@gmail.com>
2023-02-20 21:40:25 -0500

    url change

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,1 +1,4 @@
-export const host = "http://localhost:8080/api";
+const useLocal = false;
+const local = "http://localhost:8080/api";
+const aws = "http://ec2-3-141-164-5.us-east-2.compute.amazonaws.com:8080/api";
+export const host = useLocal ? local : aws;
