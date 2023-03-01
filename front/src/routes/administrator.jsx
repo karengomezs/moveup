ff2e429c81ee7e3f43070d8fb8106fb9bc3ba90f
karengomezs <karengomezsoto1@gmail.com>
2023-03-01 13:57:20 -0500

    created new route, div administrator

--- front/src/routes/administrator.jsx
+++ front/src/routes/administrator.jsx
@@ -58,176 +58,185 @@ export default function Administrator() {
   });
 
   return (
-    <div className="container flex-grow-1">
-      <P className=" fs-4 fw-bold mt-5">Crear Clase</P>
-      <CARD className="container d-flex flex-column mb-5">
-        <form
-          className="card-body"
-          onSubmit={(e) => {
-            e.preventDefault();
-            // console.log(city, category, name, description, adress);
+    <>
+      <div
+        className={`w-100 p-2 ${themeState.theme ? "bg-search" : "bg-light"}`}
+      >
+        <P className="container fs-4 fw-bold text-primary my-auto">
+          Administración
+        </P>
+      </div>
+      <div className="container flex-grow-1">
+        <P className=" fs-4 fw-bold mt-5">Crear Clase</P>
+        <CARD className="container d-flex flex-column mb-5">
+          <form
+            className="card-body"
+            onSubmit={(e) => {
+              e.preventDefault();
+              // console.log(city, category, name, description, adress);
 
-            const isNameValid = name.length > 3;
-            const isCategoryValid = category.length > 0;
-            const isAdressValid = adress.length > 3;
-            const isCityValid = city.length > 0;
-            const isDescriptionValid = description.length > 5;
+              const isNameValid = name.length > 3;
+              const isCategoryValid = category.length > 0;
+              const isAdressValid = adress.length > 3;
+              const isCityValid = city.length > 0;
+              const isDescriptionValid = description.length > 5;
 
-            if (
-              isNameValid &&
-              isCategoryValid &&
-              isAdressValid &&
-              isCityValid &&
-              isDescriptionValid
-            ) {
-              try {
-                // acá va la consulta del response
-              } catch (error) {
-                setErrorPostProduct(true);
+              if (
+                isNameValid &&
+                isCategoryValid &&
+                isAdressValid &&
+                isCityValid &&
+                isDescriptionValid
+              ) {
+                try {
+                  // acá va la consulta del response
+                } catch (error) {
+                  setErrorPostProduct(true);
+                }
+              } else {
+                setNameError(!isNameValid);
+                setCategoryError(!isCategoryValid);
+                setAdressError(!isAdressValid);
+                setCityError(!isCityValid);
+                setDescriptionError(!isDescriptionValid);
               }
-            } else {
-              setNameError(!isNameValid);
-              setCategoryError(!isCategoryValid);
-              setAdressError(!isAdressValid);
-              setCityError(!isCityValid);
-              setDescriptionError(!isDescriptionValid);
-            }
-          }}
-        >
-          {/*1. DIV PRIMEROS DOS INPUTS */}
-          <div className="d-sm-flex gap-3">
-            <div className="mb-3 flex-grow-1">
-              <LABEL className="fw-semibold" htmlFor="name">
-                Nombre de la clase
-              </LABEL>
-              <INPUT
-                onChange={(e) => {
-                  setName(e.target.value);
-                  setNameError(false);
-                }}
-                value={name}
-                id="name"
-                type="text"
-                className={`form-control ${nameError ? "is-invalid" : ""}`}
-                placeholder="Ejemplo: Taekwondo"
-              />
-
-              <div className="invalid-feedback">
-                Por favor ingrese más de 3 caracteres
-              </div>
-            </div>
-            <div className="mb-3 flex-grow-1">
-              <LABEL className="fw-semibold" htmlFor="last-name">
-                Categoría
-              </LABEL>
-              <SELECT
-                onChange={(e) => {
-                  setCategory(e.target.value);
-                  setCategoryError(false);
-                }}
-                value={category}
-                className={`${categoryError ? "is-invalid" : ""} `}
-              >
-                <option value="">Elige una categoría</option>
-                {categories}
-              </SELECT>
-              {categoryError && (
-                <div className="invalid-feedback">
-                  Por favor seleccione una categoría
-                </div>
-              )}
-            </div>
-          </div>
-          {/* 1. FIN */}
-          {/*2.  DIV SEGUNDOS DOS INPUTS  */}
-          <div className="d-sm-flex gap-3">
-            <div className="mb-3 flex-grow-1">
-              <LABEL className="fw-semibold" htmlFor="name">
-                Dirección
-              </LABEL>
-              <INPUT
-                onChange={(e) => {
-                  setAdress(e.target.value);
-                }}
-                value={adress}
-                id="name"
-                type="text"
-                className={`form-control ${adressError ? "is-invalid" : ""}`}
-                placeholder="Ejemplo: cll 43 # 34-56"
-              />
-
-              <div className="invalid-feedback">
-                Por favor ingrese más de 3 caracteres
-              </div>
-            </div>
-            <div className="mb-3 flex-grow-1">
-              <LABEL className="fw-semibold" htmlFor="last-name">
-                Ciudad
-              </LABEL>
-              <SELECT
-                onChange={(e) => {
-                  setCity(e.target.value);
-                  setCityError(false);
-                }}
-                value={city}
-                className={`${cityError ? "is-invalid" : ""}  `}
-              >
-                <option value="">Elige una ciudad</option>
-                {cities}
-              </SELECT>
-              {cityError && (
-                <div className="invalid-feedback">
-                  Por favor seleccione una ciudad
-                </div>
-              )}
-            </div>
-          </div>
-          {/* 1. FIN */}
-          {/* 3. DIV TEXT AREA */}
-          <div>
-            <LABEL className="fw-semibold" htmlFor="last-name">
-              Descripción
-            </LABEL>
-            <textarea
-              onChange={(e) => {
-                setDescription(e.target.value);
-                setDescriptionError(false);
-              }}
-              value={description}
-              id="name"
-              type="text-area"
-              className={`form-control ${
-                descriptionError ? "is-invalid" : ""
-              } ${themeState.theme ? "text-bg-dark" : ""}`}
-              placeholder="Ejemplo: Clase con movilidad de artes marciales y defensa personal avanzada"
-            ></textarea>
-            <div className="invalid-feedback">
-              Por favor ingrese más de 5 caracteres
-            </div>
-          </div>
-          {/* 3. FIN TEXT AREA */}
-          {/*           <P className="fw-bold mt-3">Agregar atributos</P>
-           */}
-          <ButtonOutlinePrimary
-            type="submit"
-            className="mx-auto d-block mb-3 mt-5 w-50"
+            }}
           >
-            Crear Producto
-          </ButtonOutlinePrimary>
-        </form>
-      </CARD>
-      {errorPostProduct && (
-        <div
-          className="alert alert-danger d-flex align-items-center mb-4"
-          role="alert"
-        >
-          <i className="bi bi-exclamation-circle-fill me-2"></i>
-          <span>
-            Lamentablemente, el producto no ha podido crearse. Por favor,
-            intente más tarde
-          </span>
-        </div>
-      )}
-    </div>
+            {/*1. DIV PRIMEROS DOS INPUTS */}
+            <div className="d-sm-flex gap-3">
+              <div className="mb-3 w-100">
+                <LABEL className="fw-semibold" htmlFor="name">
+                  Nombre de la clase
+                </LABEL>
+                <INPUT
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setNameError(false);
+                  }}
+                  value={name}
+                  id="name"
+                  type="text"
+                  className={`form-control ${nameError ? "is-invalid" : ""}`}
+                  placeholder="Ejemplo: Taekwondo"
+                />
+
+                <div className="invalid-feedback">
+                  Por favor ingrese más de 3 caracteres
+                </div>
+              </div>
+              <div className="mb-3 w-100">
+                <LABEL className="fw-semibold" htmlFor="last-name">
+                  Categoría
+                </LABEL>
+                <SELECT
+                  onChange={(e) => {
+                    setCategory(e.target.value);
+                    setCategoryError(false);
+                  }}
+                  value={category}
+                  className={`${categoryError ? "is-invalid" : ""} `}
+                >
+                  <option value="">Elige una categoría</option>
+                  {categories}
+                </SELECT>
+                {categoryError && (
+                  <div className="invalid-feedback">
+                    Por favor seleccione una categoría
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* 1. FIN */}
+            {/*2.  DIV SEGUNDOS DOS INPUTS  */}
+            <div className="d-sm-flex gap-3">
+              <div className="mb-3 w-100">
+                <LABEL className="fw-semibold" htmlFor="name">
+                  Dirección
+                </LABEL>
+                <INPUT
+                  onChange={(e) => {
+                    setAdress(e.target.value);
+                  }}
+                  value={adress}
+                  id="name"
+                  type="text"
+                  className={`form-control ${adressError ? "is-invalid" : ""}`}
+                  placeholder="Ejemplo: cll 43 # 34-56"
+                />
+
+                <div className="invalid-feedback">
+                  Por favor ingrese más de 3 caracteres
+                </div>
+              </div>
+              <div className="mb-3 w-100">
+                <LABEL className="fw-semibold" htmlFor="last-name">
+                  Ciudad
+                </LABEL>
+                <SELECT
+                  onChange={(e) => {
+                    setCity(e.target.value);
+                    setCityError(false);
+                  }}
+                  value={city}
+                  className={`${cityError ? "is-invalid" : ""}  `}
+                >
+                  <option value="">Elige una ciudad</option>
+                  {cities}
+                </SELECT>
+                {cityError && (
+                  <div className="invalid-feedback">
+                    Por favor seleccione una ciudad
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* 1. FIN */}
+            {/* 3. DIV TEXT AREA */}
+            <div>
+              <LABEL className="fw-semibold" htmlFor="last-name">
+                Descripción
+              </LABEL>
+              <textarea
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                  setDescriptionError(false);
+                }}
+                value={description}
+                id="name"
+                type="text-area"
+                className={`form-control ${
+                  descriptionError ? "is-invalid" : ""
+                } ${themeState.theme ? "text-bg-dark" : ""}`}
+                placeholder="Ejemplo: Clase con movilidad de artes marciales y defensa personal avanzada"
+              ></textarea>
+              <div className="invalid-feedback">
+                Por favor ingrese más de 5 caracteres
+              </div>
+            </div>
+            {/* 3. FIN TEXT AREA */}
+            {/*           <P className="fw-bold mt-3">Agregar atributos</P>
+             */}
+            <ButtonOutlinePrimary
+              type="submit"
+              className="mx-auto d-block mb-3 mt-5 w-50"
+            >
+              Crear Producto
+            </ButtonOutlinePrimary>
+          </form>
+        </CARD>
+        {errorPostProduct && (
+          <div
+            className="alert alert-danger d-flex align-items-center mb-4"
+            role="alert"
+          >
+            <i className="bi bi-exclamation-circle-fill me-2"></i>
+            <span>
+              Lamentablemente, el producto no ha podido crearse. Por favor,
+              intente más tarde
+            </span>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
---
+++ front/src/routes/confirmed-create.jsx
@@ -0,0 +1,27 @@
+import { Link, useLocation, Navigate } from "react-router-dom";
+import CARD from "../components/common/card";
+
+export default function ConfirmedCreate() {
+  const location = useLocation();
+  //   const isBookingSuccess = location.state === "booking-success";
+
+  //   if (!isBookingSuccess) {
+  //     return <Navigate to="/" replace={true} />;
+  //   }
+
+  return (
+    <div className="d-flex flex-grow-1 flex-column align-items-center justify-content-center container">
+      <CARD className="shadow-sm py-3 px-5 ">
+        <div className="card-body d-flex flex-column align-items-center gap-3">
+          <i className="bi bi-patch-check-fill fs-1 text-primary"></i>
+          <p className="fs-4 fw-semibold">Su producto se ha creado con éxito</p>
+          <Link className="w-50" to="/" replace>
+            <button className="btn btn-primary w-100 fs-5 fw-semibold">
+              Ok
+            </button>
+          </Link>
+        </div>
+      </CARD>
+    </div>
+  );
+}
